2d7f98484f1acd9a22d559e7cf578a1ccff611e7
Georgy Buranov <gburanov@gmail.com>
2016-04-18 20:10:05 +0200

    modify results

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,19 +4,33 @@ import (
   "pr_helper"
 
   "log"
+  "strings"
+  "strconv"
+  "fmt"
 
   "net/http"
   "html/template"
   "github.com/go-martini/martini"
 )
 
-func showPrs() string {
-  ret := ""
-  repo := pr_helper.RepositoryFromSettings()
-  for _, pr := range repo.PRs() {
-    ret += "<p>"
-    ret += pr.Topic()
+func showPr(url string) string {
+  ret := "<p>Analyzing pr " + url
+  slices := strings.Split(url, "/")
+  fmt.Println(slices)
+  num, err := strconv.Atoi(slices[len(slices)-1])
+  if err != nil {
+    ret += "<p>Unable to find number " + slices[len(slices)-1]
+    return ret
   }
+
+  repo := pr_helper.RepositoryFromSettings()
+  pr, err := repo.GetPR(num)
+  if err != nil {
+    ret += "<p>Unable to find pr " + strconv.Itoa(num)
+    return ret
+  }
+  ret += "<p>"
+  ret += pr.Topic()
 
   return ret
 }
@@ -32,8 +46,8 @@ func main() {
   })
 
   m.Post("/results", func(r *http.Request) string {
-        text := r.FormValue("url")
-        return text
+      url := r.FormValue("url")
+      return showPr(url)
     })
   m.Run()
 }
