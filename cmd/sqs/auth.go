9b04f09ef8dd5eac16cf29217d0ec162aaa02a5f
Georgy Buranov <gburanov@gmail.com>
2016-08-06 17:10:24 +0200

    some optimisations

--- cmd/sqs/auth.go
+++ cmd/sqs/auth.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-  "os"
+	"os"
 
-  "github.com/goamz/goamz/aws"
+	"github.com/goamz/goamz/aws"
 )
 
 var auth = aws.Auth{
-  AccessKey: os.Getenv("AWS_PRIVATE_ACCESS_KEY_ID"),
-  SecretKey: os.Getenv("AWS_PRIVATE_SECRET_ACCESS_KEY"),
+	AccessKey: os.Getenv("AWS_PRIVATE_ACCESS_KEY_ID"),
+	SecretKey: os.Getenv("AWS_PRIVATE_SECRET_ACCESS_KEY"),
 }
--- cmd/sqs/queue.go
+++ cmd/sqs/queue.go
@@ -1,54 +1,61 @@
 package main
 
 import (
-  "log"
-  "fmt"
+	"fmt"
+	"log"
 
-  "github.com/goamz/goamz/aws"
-  "github.com/goamz/goamz/sqs"
+	"github.com/goamz/goamz/aws"
+	"github.com/goamz/goamz/sqs"
 )
 
+func getQueue() (*sqs.Queue, error) {
+	if len(auth.AccessKey) == 0 || len(auth.SecretKey) == 0 {
+		return nil, fmt.Errorf("AWS credentials are not provided")
+	}
+	conn := sqs.New(auth, aws.EUWest)
+	return conn.CreateQueue("pr_helper_input")
+}
+
 func ReadMessage() sqs.Message {
-  conn := sqs.New(auth, aws.EUWest)
-  q, err := conn.CreateQueue("pr_helper_input")
-  if err != nil {
-    log.Fatal(err)
-  }
-  fmt.Println("Waiting for messages...")
-  for {
-    resp, err := q.ReceiveMessage(1)
-    if err != nil {
-      log.Print(err)
-      continue
-    }
-    if len(resp.Messages) != 0 {
-      return resp.Messages[0]
-    }
-  }
+	fmt.Println("Waiting for messages...")
+	q, err := getQueue()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		resp, err := q.ReceiveMessage(1)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		if len(resp.Messages) != 0 {
+			return resp.Messages[0]
+		}
+	}
 }
 
 func DeleteMessage(m sqs.Message) {
-  conn := sqs.New(auth, aws.EUWest)
-  q, err := conn.CreateQueue("pr_helper_input")
-  if err != nil {
-    log.Fatal(err)
-  }
-  _, err = q.DeleteMessage(&m)
-  if err != nil {
-    log.Fatal(err)
-  }
+	conn := sqs.New(auth, aws.EUWest)
+	q, err := conn.CreateQueue("pr_helper_input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = q.DeleteMessage(&m)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func SendMessage(message string, uuid string) {
-  conn := sqs.New(auth, aws.EUWest)
-  q, err := conn.CreateQueue("pr_helper_output")
-  if err != nil {
-    log.Fatal(err)
-  }
-  m := map[string]string{}
-  m["uuid"] = uuid
-  _, err = q.SendMessageWithAttributes(message, m)
-  if err != nil {
-    log.Fatal(err)
-  }
+	conn := sqs.New(auth, aws.EUWest)
+	q, err := conn.CreateQueue("pr_helper_output")
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := map[string]string{}
+	m["uuid"] = uuid
+	_, err = q.SendMessageWithAttributes(message, m)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
