cf61de01aed476270ca076857bbe04c55118519f
Georgy Buranov <gburanov@gmail.com>
2016-08-06 17:00:04 +0200

    style checks

--- cmd/sqs/main.go
+++ cmd/sqs/main.go
@@ -1,43 +1,44 @@
 package main
 
 import (
-  "fmt"
-  "pr_helper/lib"
-  "log"
+	"fmt"
+	"log"
+
+	"github.com/gburanov/pr_helper/lib"
 )
 
 func ProcessUrl(url string, cb pr_helper.Callback, m *pr_helper.Mutex) {
-  showPr(url, cb, m)
-  cb("END OF MESSAGE")
+	showPr(url, cb, m)
+	cb("END OF MESSAGE")
 }
 
 func messageCallback(uuid string) pr_helper.Callback {
-    return func(str string, args ...interface{}) {
-        message := fmt.Sprintf(str, args...)
-        fmt.Println(message)
-        SendMessage(message, uuid)
-    }
+	return func(str string, args ...interface{}) {
+		message := fmt.Sprintf(str, args...)
+		fmt.Println(message)
+		SendMessage(message, uuid)
+	}
 }
 
 func main() {
-  log.SetFlags(log.LstdFlags | log.Lshortfile)
-  m := pr_helper.NewMutex()
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	m := pr_helper.NewMutex()
 
-  for {
-    message := ReadMessage()
-    var uuid string
-    for _, attr := range message.MessageAttribute {
-      if attr.Name == "uuid" {
-        uuid = attr.Value.StringValue
-        break
-      }
-    }
-    if len(uuid) == 0 {
-      fmt.Println("Invalid message processed")
-    } else {
-      callback := messageCallback(uuid)
-      go ProcessUrl(message.Body, callback, m)
-    }
-    DeleteMessage(message)
-  }
+	for {
+		message := ReadMessage()
+		var uuid string
+		for _, attr := range message.MessageAttribute {
+			if attr.Name == "uuid" {
+				uuid = attr.Value.StringValue
+				break
+			}
+		}
+		if len(uuid) == 0 {
+			fmt.Println("Invalid message processed")
+		} else {
+			callback := messageCallback(uuid)
+			go ProcessUrl(message.Body, callback, m)
+		}
+		DeleteMessage(message)
+	}
 }
--- cmd/sqs/show_pr.go
+++ cmd/sqs/show_pr.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-  "pr_helper/lib"
+	"github.com/gburanov/pr_helper/lib"
 )
 
 func showPr(url string, cb pr_helper.Callback, m *pr_helper.Mutex) {
-  cb("Analyzing pr %s", url)
-  manager := pr_helper.NewManager(cb, m)
-  pr, err := manager.GetPR(url)
-  if err != nil {
-    cb(err.Error())
-    return
-  }
-  cb(pr.Topic())
-  showAuthors(*pr, cb)
+	cb("Analyzing pr %s", url)
+	manager := pr_helper.NewManager(cb, m)
+	pr, err := manager.GetPR(url)
+	if err != nil {
+		cb(err.Error())
+		return
+	}
+	cb(pr.Topic())
+	showAuthors(*pr, cb)
 }
 
 func showLeftStats(authors *pr_helper.Authors, cb pr_helper.Callback) {
-  left, total := authors.GetLinesStat()
-  percent := float32(left)/float32(total)
-  if left == 0 {
-    // does not make sense
-    return
-  }
+	left, total := authors.GetLinesStat()
+	percent := float32(left) / float32(total)
+	if left == 0 {
+		// does not make sense
+		return
+	}
 
-  cb("%d out of %d lines unmntained", left, total)
-  if (total > 100 && percent > 0.7) || (percent > 0.9) {
-    cb("WARNING! DEEP LEGACY")
-  }
+	cb("%d out of %d lines unmntained", left, total)
+	if (total > 100 && percent > 0.7) || (percent > 0.9) {
+		cb("WARNING! DEEP LEGACY")
+	}
 }
 
 func showAuthors(pr pr_helper.PR, cb pr_helper.Callback) {
-  stats := pr.Stats()
-  cb("Total files %d", stats.FilesCount)
-  cb("Total lines %d", stats.Lines())
-  cb("Average time %s", stats.AverageTime().String())
-  cb("Earliest time %s", stats.EarliestTime().String())
-  authors := pr_helper.CreateAuthors(stats)
-  showLeftStats(authors, cb)
-  for author, lines := range *pr_helper.FilterTop(5, authors) {
-    cb("%s (%d lines changed)", author.AsStr(), lines)
-  }
+	stats := pr.Stats()
+	cb("Total files %d", stats.FilesCount)
+	cb("Total lines %d", stats.Lines())
+	cb("Average time %s", stats.AverageTime().String())
+	cb("Earliest time %s", stats.EarliestTime().String())
+	authors := pr_helper.CreateAuthors(stats)
+	showLeftStats(authors, cb)
+	for author, lines := range *pr_helper.FilterTop(5, authors) {
+		cb("%s (%d lines changed)", author.AsStr(), lines)
+	}
 }
